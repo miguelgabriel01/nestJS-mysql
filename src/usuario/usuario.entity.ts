28888d96b3104eed3bfe1072fdc481bbc509f8f0
miguelgabriel01 <gabrielogabriel10@gmail.com>
2022-06-27 03:01:47 -0300

    inicio das configurações de banco de dados

---
+++ src/usuario/usuario.entity.ts
@@ -0,0 +1,16 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Usuario {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 100 })
+  nome: string;
+
+  @Column({ length: 100 })
+  email: string;
+
+  @Column({ length: 20 })
+  password: string;
+}
---
+++ src/usuario/usuario.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { DatabaseModule } from '../database/database.module';
+import { usuarioProviders } from './usuario.providers';
+import { UsuarioService } from './usuario.service';
+
+@Module({
+  imports: [DatabaseModule],
+  providers: [...usuarioProviders, UsuarioService],
+})
+export class UsuarioModule {}
